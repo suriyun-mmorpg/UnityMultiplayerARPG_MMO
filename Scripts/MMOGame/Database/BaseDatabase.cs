ffc2c8e640f15b40a3e2818aba4ef89c513fcd0c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-05-22 18:23:15 +0700

    Add Draft Database

---
+++ Scripts/MMOGame/Database/BaseDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Insthync.MMOG
+{
+    public abstract class BaseDatabase : MonoBehaviour
+    {
+        public abstract bool ValidateLogin(string username, string password);
+        public abstract UserLoginData Register(string username, string password);
+        public abstract long FindUsername(string username);
+        public abstract bool CreateCharacter(string userId, PlayerCharacterData characterData);
+        public abstract PlayerCharacterData ReadCharacter(string characterId);
+        public abstract List<LitePlayerCharacterData> ReadCharacters(string userId);
+        public abstract bool UpdateCharacter(PlayerCharacterData characterData);
+        public abstract bool DeleteCharacter(string characterId);
+        public abstract long FindCharacterName(string characterName);
+    }
+}
---
+++ Scripts/MMOGame/Database/UserLoginData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Insthync.MMOG
+{
+    public class UserLoginData
+    {
+        public string id;
+        public string username;
+        public string password;
+        public int lastUpdate;
+    }
+}
