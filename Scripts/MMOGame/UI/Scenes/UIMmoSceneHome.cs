b4dcfc48db4e9e643f8f9971382c05055cf3291c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-06-06 18:26:53 +0700

    Add scene management ui

---
+++ Scripts/MMOGame/UI/Scenes/UIMmoSceneHome.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LiteNetLib;
+
+namespace Insthync.MMOG
+{
+    public class UIMmoSceneHome : UIHistory
+    {
+        public UIMmoServerList uiServerList;
+        public UIBase uiConnectedToServer;
+        public UIMmoCharacterCreate uiCharacterCreate;
+        public UIMmoCharacterList uiCharacterList;
+
+        private void OnEnable()
+        {
+            MMOClientInstance.Singleton.onClientConnected += OnCentralServerConnected;
+            MMOClientInstance.Singleton.onClientDisconnected += OnCentralServerDisconnected;
+        }
+
+        private void OnDisable()
+        {
+            MMOClientInstance.Singleton.onClientConnected -= OnCentralServerConnected;
+            MMOClientInstance.Singleton.onClientDisconnected -= OnCentralServerDisconnected;
+        }
+
+        public void OnCentralServerConnected(NetPeer netPeer)
+        {
+            Next(uiConnectedToServer);
+        }
+
+        public void OnCentralServerDisconnected(NetPeer netPeer, DisconnectInfo disconnectInfo)
+        {
+            ClearHistory();
+        }
+
+        public void OnClickExit()
+        {
+            Application.Quit();
+        }
+    }
+}
