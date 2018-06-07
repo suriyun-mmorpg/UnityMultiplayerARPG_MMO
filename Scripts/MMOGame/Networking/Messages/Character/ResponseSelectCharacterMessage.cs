f8e1d576e232a4c2e813aa4838c15b2d9431e208
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-06-08 03:37:59 +0700

    Add map server data when response character selection

--- Scripts/MMOGame/Networking/Messages/Character/ResponseSelectCharacterMessage.cs
+++ Scripts/MMOGame/Networking/Messages/Character/ResponseSelectCharacterMessage.cs
@@ -12,19 +12,31 @@ namespace Insthync.MMOG
             None,
             NotLoggedin,
             AlreadySelectCharacter,
-            InvalidCharacterId,
+            InvalidCharacterData,
             MapNotReady,
         }
         public Error error;
+        public string sceneName;
+        public string networkAddress;
+        public int networkPort;
+        public string connectKey;
 
         public override void DeserializeData(NetDataReader reader)
         {
             error = (Error)reader.GetByte();
+            sceneName = reader.GetString();
+            networkAddress = reader.GetString();
+            networkPort = reader.GetInt();
+            connectKey = reader.GetString();
         }
 
         public override void SerializeData(NetDataWriter writer)
         {
             writer.Put((byte)error);
+            writer.Put(sceneName);
+            writer.Put(networkAddress);
+            writer.Put(networkPort);
+            writer.Put(connectKey);
         }
     }
 }
