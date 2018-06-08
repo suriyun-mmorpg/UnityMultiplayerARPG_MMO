6cde05bbedeb5b60a2e6533c57d5fd3e9fd3e11e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-06-09 02:35:34 +0700

    Add map spawn message handlers

--- Scripts/MMOGame/Networking/Central/CentralNetworkManager_MapSpawn.cs
+++ Scripts/MMOGame/Networking/Central/CentralNetworkManager_MapSpawn.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using LiteNetLib;
+using LiteNetLibManager;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +8,26 @@ namespace Insthync.MMOG
 {
     public partial class CentralNetworkManager
     {
-        protected void SetupMapSpawn(CentralServerPeerInfo peerInfo)
-        {
 
+        public uint RequestSpawnMap(NetPeer peer, string sceneName, AckMessageCallback callback)
+        {
+            var message = new RequestSpawnMapMessage();
+            message.sceneName = sceneName;
+            return Server.SendAckPacket(SendOptions.ReliableUnordered, peer, MessageTypes.RequestSpawnMap, message, callback);
+        }
+
+        protected void HandleResponseSpawnMap(LiteNetLibMessageHandler messageHandler)
+        {
+            var peerHandler = messageHandler.peerHandler;
+            var peer = messageHandler.peer;
+            var message = messageHandler.ReadMessage<ResponseSpawnMapMessage>();
+            var ackId = message.ackId;
+            peerHandler.TriggerAck(ackId, message.responseCode, message);
+        }
+
+        protected void OnRequestSpawnMap(AckResponseCode responseCode, BaseAckMessage messageData)
+        {
+            Debug.Log("Spawn Map Ack Id: " + messageData.ackId + "  Status " + responseCode);
         }
     }
 }
