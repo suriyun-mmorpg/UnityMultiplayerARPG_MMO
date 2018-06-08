108e3a500a14d46ec9ceb4b35f9e445130eaca69
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-06-09 02:34:21 +0700

    Use ack id instead of generating spawn id

--- Scripts/MMOGame/Networking/Messages/MapSpawn/RequestSpawnMapMessage.cs
+++ Scripts/MMOGame/Networking/Messages/MapSpawn/RequestSpawnMapMessage.cs
@@ -7,18 +7,15 @@ namespace Insthync.MMOG
 {
     public class RequestSpawnMapMessage : BaseAckMessage
     {
-        public int spawnId;
         public string sceneName;
 
         public override void DeserializeData(NetDataReader reader)
         {
-            spawnId = reader.GetInt();
             sceneName = reader.GetString();
         }
 
         public override void SerializeData(NetDataWriter writer)
         {
-            writer.Put(spawnId);
             writer.Put(sceneName);
         }
     }
