a106e72a360b11f05524ecfb1284385010824b3a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-06-08 21:42:55 +0700

    WIP Map Spawn Server

---
+++ Scripts/MMOGame/Networking/Messages/MapSpawn/ResponseSpawnMapMessage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LiteNetLibManager;
+using LiteNetLib.Utils;
+
+namespace Insthync.MMOG
+{
+    public class ResponseSpawnMapMessage : BaseAckMessage
+    {
+        public enum Error : byte
+        {
+            None,
+            NotReady,
+            Unauthorized,
+            EmptySceneName,
+            CannotExecute,
+            Unknow,
+        }
+        public Error error;
+
+        public override void DeserializeData(NetDataReader reader)
+        {
+            error = (Error)reader.GetByte();
+        }
+
+        public override void SerializeData(NetDataWriter writer)
+        {
+            writer.Put((byte)error);
+        }
+    }
+}
