ef5506f5bd5d82ca471ddd5f116bb848775386a2
insthync <ittipon.bay@gmail.com>
2018-07-12 08:32:06 +0700

    Add central ack loading UI

---
+++ Scripts/MMOGame/UI/Scenes/UIMmoCentralAckLoading.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG.MMO
+{
+    public class UIMmoCentralAckLoading : MonoBehaviour
+    {
+        public static UIMmoCentralAckLoading Singleton { get; private set; }
+        public GameObject rootObject;
+
+        private void Awake()
+        {
+            if (Singleton != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            DontDestroyOnLoad(gameObject);
+            Singleton = this;
+
+            if (rootObject != null)
+                rootObject.SetActive(false);
+
+            InvokeRepeating("UpdateUI", 0.5f, 0.5f);
+        }
+
+        void UpdateUI()
+        {
+            if (rootObject != null)
+                rootObject.SetActive(MMOClientInstance.Singleton.CentralNetworkManager.IsClientConnected && MMOClientInstance.Singleton.CentralNetworkManager.Client.AckCallbacksCount > 0);
+        }
+    }
+}
