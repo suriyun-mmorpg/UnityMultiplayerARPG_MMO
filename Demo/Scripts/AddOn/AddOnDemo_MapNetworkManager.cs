7f2da86b2396581c36719e328b96f740e04ec845
insthync <ittipon.bay@gmail.com>
2018-07-24 07:16:16 +0700

    Update demo content

---
+++ Demo/Scripts/AddOn/AddOnDemo_MapNetworkManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG.MMO
+{
+    public partial class MapNetworkManager
+    {
+        [Header("Demo Addon")]
+        public bool writeAddonLog;
+        protected void AddOnDemo_RegisterClientMessages()
+        {
+            if (writeAddonLog) Debug.Log("[" + name + "] LanRpgNetworkManager.RegisterClientMessages()");
+        }
+
+        protected void AddOnDemo_RegisterServerMessages()
+        {
+            if (writeAddonLog) Debug.Log("[" + name + "] LanRpgNetworkManager.RegisterServerMessages()");
+        }
+
+        protected void AddOnDemo_Init()
+        {
+            if (writeAddonLog) Debug.Log("[" + name + "] LanRpgNetworkManager.Init()");
+        }
+
+        protected void AddOnDemo_OnClientOnlineSceneLoaded()
+        {
+            if (writeAddonLog) Debug.Log("[" + name + "] LanRpgNetworkManager.OnClientOnlineSceneLoaded()");
+        }
+
+        protected void AddOnDemo_OnServerOnlineSceneLoaded()
+        {
+            if (writeAddonLog) Debug.Log("[" + name + "] LanRpgNetworkManager.OnServerOnlineSceneLoaded()");
+        }
+    }
+}
