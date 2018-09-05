81e3b64d956ac4359664a0ab15145306d929c621
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-09-05 17:42:59 +0700

    Use ushort for message type instead of short

--- Scripts/MMOGame/Networking/Messages/MMOMessageTypes.cs
+++ Scripts/MMOGame/Networking/Messages/MMOMessageTypes.cs
@@ -2,31 +2,31 @@
 {
     public class MMOMessageTypes
     {
-        public const short RequestAppServerRegister = 0;
-        public const short ResponseAppServerRegister = 1;
-        public const short RequestAppServerAddress = 2;
-        public const short ResponseAppServerAddress = 3;
-        public const short RequestUserLogin = 4;
-        public const short ResponseUserLogin = 5;
-        public const short RequestUserRegister = 6;
-        public const short ResponseUserRegister = 7;
-        public const short RequestUserLogout = 8;
-        public const short ResponseUserLogout = 9;
-        public const short RequestCharacters = 10;
-        public const short ResponseCharacters = 11;
-        public const short RequestCreateCharacter = 12;
-        public const short ResponseCreateCharacter = 13;
-        public const short RequestDeleteCharacter = 14;
-        public const short ResponseDeleteCharacter = 15;
-        public const short RequestSelectCharacter = 16;
-        public const short ResponseSelectCharacter = 17;
-        public const short RequestSpawnMap = 18;
-        public const short ResponseSpawnMap = 19;
-        public const short RequestValidateAccessToken = 20;
-        public const short ResponseValidateAccessToken = 21;
-        public const short UpdateMapUser = 22;
-        public const short Chat = 23;
-        public const short RequestFacebookLogin = 24;
-        public const short RequestGooglePlayLogin = 25;
+        public const ushort RequestAppServerRegister = 0;
+        public const ushort ResponseAppServerRegister = 1;
+        public const ushort RequestAppServerAddress = 2;
+        public const ushort ResponseAppServerAddress = 3;
+        public const ushort RequestUserLogin = 4;
+        public const ushort ResponseUserLogin = 5;
+        public const ushort RequestUserRegister = 6;
+        public const ushort ResponseUserRegister = 7;
+        public const ushort RequestUserLogout = 8;
+        public const ushort ResponseUserLogout = 9;
+        public const ushort RequestCharacters = 10;
+        public const ushort ResponseCharacters = 11;
+        public const ushort RequestCreateCharacter = 12;
+        public const ushort ResponseCreateCharacter = 13;
+        public const ushort RequestDeleteCharacter = 14;
+        public const ushort ResponseDeleteCharacter = 15;
+        public const ushort RequestSelectCharacter = 16;
+        public const ushort ResponseSelectCharacter = 17;
+        public const ushort RequestSpawnMap = 18;
+        public const ushort ResponseSpawnMap = 19;
+        public const ushort RequestValidateAccessToken = 20;
+        public const ushort ResponseValidateAccessToken = 21;
+        public const ushort UpdateMapUser = 22;
+        public const ushort Chat = 23;
+        public const ushort RequestFacebookLogin = 24;
+        public const ushort RequestGooglePlayLogin = 25;
     }
 }
