f9657b650af028c64a3bc0bb01bd7a5f6459406d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-12-06 22:46:59 +0700

    Change from bool to tinyint

--- SQLs/mysql_update_1.35-1.36.sql
+++ SQLs/mysql_update_1.35-1.36.sql
@@ -1,6 +1,6 @@
 START TRANSACTION;
 
-ALTER TABLE `characteritem` ADD `isSummoned` BOOLEAN NOT NULL DEFAULT FALSE AFTER `durability`, ADD `currentSummonedHp` INT NOT NULL DEFAULT '0' AFTER `isSummoned`, ADD `currentSummonedMp` INT NOT NULL DEFAULT '0' AFTER `currentSummonedHp`, ADD `currentSummonedExp` INT NOT NULL DEFAULT '0' AFTER `currentSummonedMp`;
-ALTER TABLE `characterskillusage` ADD `isSummoned` BOOLEAN NOT NULL DEFAULT FALSE AFTER `coolDownRemainsDuration`, ADD `currentSummonedHp` INT NOT NULL DEFAULT '0' AFTER `isSummoned`, ADD `currentSummonedMp` INT NOT NULL DEFAULT '0' AFTER `currentSummonedHp`
+ALTER TABLE `characteritem` ADD `isSummoned` TINYINT(1) NOT NULL DEFAULT 0 AFTER `durability`, ADD `currentSummonedHp` INT NOT NULL DEFAULT '0' AFTER `isSummoned`, ADD `currentSummonedMp` INT NOT NULL DEFAULT '0' AFTER `currentSummonedHp`, ADD `currentSummonedExp` INT NOT NULL DEFAULT '0' AFTER `currentSummonedMp`;
+ALTER TABLE `characterskillusage` ADD `isSummoned` TINYINT(1) NOT NULL DEFAULT 0 AFTER `coolDownRemainsDuration`, ADD `currentSummonedHp` INT NOT NULL DEFAULT '0' AFTER `isSummoned`, ADD `currentSummonedMp` INT NOT NULL DEFAULT '0' AFTER `currentSummonedHp`;
 
 COMMIT;
