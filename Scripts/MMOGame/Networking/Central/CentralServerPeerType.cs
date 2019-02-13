9c6599b3f8c1025e0b3558e17c91d2929da8f7fc
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-02-13 18:51:24 +0700

    Remove unused namespaces

--- Scripts/MMOGame/Networking/Central/CentralServerPeerType.cs
+++ Scripts/MMOGame/Networking/Central/CentralServerPeerType.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using LiteNetLib;
-
-namespace MultiplayerARPG.MMO
+﻿namespace MultiplayerARPG.MMO
 {
     public enum CentralServerPeerType : byte
     {
