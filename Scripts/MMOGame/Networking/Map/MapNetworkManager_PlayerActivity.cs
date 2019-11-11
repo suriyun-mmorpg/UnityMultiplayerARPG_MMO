00d3b066bdc853151dba7103fd99d2d3ddc97495
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-11-12 01:35:26 +0700

    Fix invalid move to / from storage logics

--- Scripts/MMOGame/Networking/Map/MapNetworkManager_PlayerActivity.cs
+++ Scripts/MMOGame/Networking/Map/MapNetworkManager_PlayerActivity.cs
@@ -640,7 +640,6 @@ namespace MultiplayerARPG.MMO
                 movingItem.amount = amount;
                 if (storageItemIndex < 0 ||
                     storageItemIndex >= storageItemList.Count ||
-                    storageItemList[storageItemIndex].IsEmptySlot() ||
                     storageItemList[storageItemIndex].dataId == movingItem.dataId)
                 {
                     // Add to storage or merge
@@ -709,7 +708,6 @@ namespace MultiplayerARPG.MMO
                 movingItem.amount = amount;
                 if (nonEquipIndex < 0 ||
                     nonEquipIndex >= playerCharacterEntity.NonEquipItems.Count ||
-                    playerCharacterEntity.NonEquipItems[nonEquipIndex].IsEmptySlot() ||
                     playerCharacterEntity.NonEquipItems[nonEquipIndex].dataId == movingItem.dataId)
                 {
                     // Add to inventory or merge
