8aa9dff91e7a2de3e511cdc5f592a66538cb1f4b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-03-11 23:15:52 +0700

    - Move google play login codes to https://github.com/insthync/MMORPGKIT_GooglePlayAuth - Move facebook login codes to https://github.com/insthync/MMORPGKIT_FacebookAuth

--- Scripts/MMOGame/Networking/DatabaseJobs.cs
+++ Scripts/MMOGame/Networking/DatabaseJobs.cs
@@ -117,38 +117,6 @@ namespace MultiplayerARPG.MMO
         }
     }
 
-    public class FacebookLoginJob : DatabaseJob<string>
-    {
-        private string id;
-        private string email;
-        public FacebookLoginJob(BaseDatabase database, string id, string email, Action<string> onFinished = null) : base(database, onFinished)
-        {
-            this.id = id;
-            this.email = email;
-        }
-
-        protected override void ThreadFunction()
-        {
-            result = database.FacebookLogin(id, email);
-        }
-    }
-
-    public class GooglePlayLoginJob : DatabaseJob<string>
-    {
-        private string id;
-        private string email;
-        public GooglePlayLoginJob(BaseDatabase database, string id, string email, Action<string> onFinished = null) : base(database, onFinished)
-        {
-            this.id = id;
-            this.email = email;
-        }
-
-        protected override void ThreadFunction()
-        {
-            result = database.GooglePlayLogin(id, email);
-        }
-    }
-
     public class GetUserLevelJob : DatabaseJob<byte>
     {
         private string userId;
