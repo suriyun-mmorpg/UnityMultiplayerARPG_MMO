6c958939103cc11aba1cd0e887dfc19ce6fab532
Insthync <ittipon.bay@gmail.com>
2020-08-16 06:39:16 +0700

    Change sync functions to async

--- Scripts/MMOGame/Database/BaseDatabase.cs
+++ Scripts/MMOGame/Database/BaseDatabase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace MultiplayerARPG.MMO
@@ -10,19 +11,19 @@ namespace MultiplayerARPG.MMO
         public virtual void Initialize() { }
         public virtual void Destroy() { }
 
-        public abstract string ValidateUserLogin(string username, string password);
-        public abstract bool ValidateAccessToken(string userId, string accessToken);
-        public abstract byte GetUserLevel(string userId);
-        public abstract int GetGold(string userId);
-        public abstract void UpdateGold(string userId, int amount);
-        public abstract int GetCash(string userId);
-        public abstract void UpdateCash(string userId, int amount);
-        public abstract void UpdateAccessToken(string userId, string accessToken);
-        public abstract void CreateUserLogin(string username, string password);
-        public abstract long FindUsername(string username);
+        public abstract Task<string> ValidateUserLogin(string username, string password);
+        public abstract Task<bool> ValidateAccessToken(string userId, string accessToken);
+        public abstract Task<byte> GetUserLevel(string userId);
+        public abstract Task<int> GetGold(string userId);
+        public abstract Task UpdateGold(string userId, int amount);
+        public abstract Task<int> GetCash(string userId);
+        public abstract Task UpdateCash(string userId, int amount);
+        public abstract Task UpdateAccessToken(string userId, string accessToken);
+        public abstract Task CreateUserLogin(string username, string password);
+        public abstract Task<long> FindUsername(string username);
 
-        public abstract void CreateCharacter(string userId, IPlayerCharacterData characterData);
-        public abstract PlayerCharacterData ReadCharacter(
+        public abstract Task CreateCharacter(string userId, IPlayerCharacterData characterData);
+        public abstract Task<PlayerCharacterData> ReadCharacter(
             string id,
             bool withEquipWeapons = true,
             bool withAttributes = true,
@@ -34,42 +35,42 @@ namespace MultiplayerARPG.MMO
             bool withSummons = true,
             bool withHotkeys = true,
             bool withQuests = true);
-        public abstract List<PlayerCharacterData> ReadCharacters(string userId);
-        public abstract void UpdateCharacter(IPlayerCharacterData character);
-        public abstract void DeleteCharacter(string userId, string id);
-        public abstract long FindCharacterName(string characterName);
-        public abstract List<SocialCharacterData> FindCharacters(string characterName);
-        public abstract void CreateFriend(string id1, string id2);
-        public abstract void DeleteFriend(string id1, string id2);
-        public abstract List<SocialCharacterData> ReadFriends(string id1);
+        public abstract Task<List<PlayerCharacterData>> ReadCharacters(string userId);
+        public abstract Task UpdateCharacter(IPlayerCharacterData character);
+        public abstract Task DeleteCharacter(string userId, string id);
+        public abstract Task<long> FindCharacterName(string characterName);
+        public abstract Task<List<SocialCharacterData>> FindCharacters(string characterName);
+        public abstract Task CreateFriend(string id1, string id2);
+        public abstract Task DeleteFriend(string id1, string id2);
+        public abstract Task<List<SocialCharacterData>> ReadFriends(string id1);
 
-        public abstract void CreateBuilding(string mapName, IBuildingSaveData saveData);
-        public abstract List<BuildingSaveData> ReadBuildings(string mapName);
-        public abstract void UpdateBuilding(string mapName, IBuildingSaveData building);
-        public abstract void DeleteBuilding(string mapName, string id);
+        public abstract Task CreateBuilding(string mapName, IBuildingSaveData saveData);
+        public abstract Task<List<BuildingSaveData>> ReadBuildings(string mapName);
+        public abstract Task UpdateBuilding(string mapName, IBuildingSaveData building);
+        public abstract Task DeleteBuilding(string mapName, string id);
 
-        public abstract int CreateParty(bool shareExp, bool shareItem, string leaderId);
-        public abstract PartyData ReadParty(int id);
-        public abstract void UpdatePartyLeader(int id, string leaderId);
-        public abstract void UpdateParty(int id, bool shareExp, bool shareItem);
-        public abstract void DeleteParty(int id);
-        public abstract void UpdateCharacterParty(string characterId, int partyId);
+        public abstract Task<int> CreateParty(bool shareExp, bool shareItem, string leaderId);
+        public abstract Task<PartyData> ReadParty(int id);
+        public abstract Task UpdatePartyLeader(int id, string leaderId);
+        public abstract Task UpdateParty(int id, bool shareExp, bool shareItem);
+        public abstract Task DeleteParty(int id);
+        public abstract Task UpdateCharacterParty(string characterId, int partyId);
 
-        public abstract int CreateGuild(string guildName, string leaderId);
-        public abstract GuildData ReadGuild(int id, GuildRoleData[] defaultGuildRoles);
-        public abstract void UpdateGuildLevel(int id, short level, int exp, short skillPoint);
-        public abstract void UpdateGuildLeader(int id, string leaderId);
-        public abstract void UpdateGuildMessage(int id, string guildMessage);
-        public abstract void UpdateGuildRole(int id, byte guildRole, string name, bool canInvite, bool canKick, byte shareExpPercentage);
-        public abstract void UpdateGuildMemberRole(string characterId, byte guildRole);
-        public abstract void UpdateGuildSkillLevel(int id, int dataId, short skillLevel, short skillPoint);
-        public abstract void DeleteGuild(int id);
-        public abstract long FindGuildName(string guildName);
-        public abstract void UpdateCharacterGuild(string characterId, int guildId, byte guildRole);
-        public abstract int GetGuildGold(int guildId);
-        public abstract void UpdateGuildGold(int guildId, int gold);
+        public abstract Task<int> CreateGuild(string guildName, string leaderId);
+        public abstract Task<GuildData> ReadGuild(int id, GuildRoleData[] defaultGuildRoles);
+        public abstract Task UpdateGuildLevel(int id, short level, int exp, short skillPoint);
+        public abstract Task UpdateGuildLeader(int id, string leaderId);
+        public abstract Task UpdateGuildMessage(int id, string guildMessage);
+        public abstract Task UpdateGuildRole(int id, byte guildRole, string name, bool canInvite, bool canKick, byte shareExpPercentage);
+        public abstract Task UpdateGuildMemberRole(string characterId, byte guildRole);
+        public abstract Task UpdateGuildSkillLevel(int id, int dataId, short skillLevel, short skillPoint);
+        public abstract Task DeleteGuild(int id);
+        public abstract Task<long> FindGuildName(string guildName);
+        public abstract Task UpdateCharacterGuild(string characterId, int guildId, byte guildRole);
+        public abstract Task<int> GetGuildGold(int guildId);
+        public abstract Task UpdateGuildGold(int guildId, int gold);
 
-        public abstract void UpdateStorageItems(StorageType storageType, string storageOwnerId, IList<CharacterItem> storageCharacterItems);
-        public abstract List<CharacterItem> ReadStorageItems(StorageType storageType, string storageOwnerId);
+        public abstract Task UpdateStorageItems(StorageType storageType, string storageOwnerId, IList<CharacterItem> storageCharacterItems);
+        public abstract Task<List<CharacterItem>> ReadStorageItems(StorageType storageType, string storageOwnerId);
     }
 }
