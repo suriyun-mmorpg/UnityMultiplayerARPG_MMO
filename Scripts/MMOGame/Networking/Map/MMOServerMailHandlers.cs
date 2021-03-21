ee317e6fe4107e9453aa9d802efa20be606a195d
Insthync <ittipon.bay@gmail.com>
2021-03-21 10:19:31 +0700

    Add and implement server mail handlers

---
+++ Scripts/MMOGame/Networking/Map/MMOServerMailHandlers.cs
@@ -0,0 +1,28 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace MultiplayerARPG.MMO
+{
+    public partial class MMOServerMailHandlers : MonoBehaviour, IServerMailHandlers
+    {
+#if UNITY_STANDALONE && !CLIENT_BUILD
+        public DatabaseService.DatabaseServiceClient DbServiceClient
+        {
+            get { return MMOServerInstance.Singleton.DatabaseNetworkManager.ServiceClient; }
+        }
+#endif
+
+        public async UniTask<bool> SendMail(Mail mail)
+        {
+#if UNITY_STANDALONE && !CLIENT_BUILD
+            SendMailResp resp = await DbServiceClient.SendMailAsync(new SendMailReq()
+            {
+                Mail = DatabaseServiceUtils.ToByteString(mail),
+            });
+            if (resp.Error == 0)
+                return true;
+#endif
+            return false;
+        }
+    }
+}
