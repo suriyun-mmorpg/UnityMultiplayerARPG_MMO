037eed28a8bb2c9b6c883c2d53488cb1f924c111
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-08-26 04:36:33 +0700

    Change type

--- Scripts/MMOGame/Networking/Map/MMOServerMailMessageHandlers.cs
+++ Scripts/MMOGame/Networking/Map/MMOServerMailMessageHandlers.cs
@@ -8,7 +8,7 @@ namespace MultiplayerARPG.MMO
     public partial class MMOServerMailMessageHandlers : MonoBehaviour, IServerMailMessageHandlers
     {
 #if UNITY_STANDALONE && !CLIENT_BUILD
-        public DatabaseNetworkManager DbServiceClient
+        public IDatabaseClient DbServiceClient
         {
             get { return MMOServerInstance.Singleton.DatabaseNetworkManager; }
         }
