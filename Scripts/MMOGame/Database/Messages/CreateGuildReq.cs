f622203bb5f89896eaadb43d322da2f1baa860f9
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-06 05:57:12 +0700

    Revert "feat: update codes follow shared codes changes"
    This reverts commit 357cf8b8fb42e30b0170f5530dfa9dd9de73cb2a.

--- Scripts/MMOGame/Database/Messages/CreateGuildReq.cs
+++ Scripts/MMOGame/Database/Messages/CreateGuildReq.cs
@@ -8,14 +8,12 @@ namespace MultiplayerARPG.MMO
         {
             GuildName = reader.GetString();
             LeaderCharacterId = reader.GetString();
-            Roles = reader.GetList<GuildRoleData>();
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.Put(GuildName);
             writer.Put(LeaderCharacterId);
-            writer.PutList(Roles);
         }
     }
 }
--- Scripts/MMOGame/Database/Messages/IncreaseGuildExpReq.cs
+++ Scripts/MMOGame/Database/Messages/IncreaseGuildExpReq.cs
@@ -7,17 +7,13 @@ namespace MultiplayerARPG.MMO
         public void Deserialize(NetDataReader reader)
         {
             GuildId = reader.GetInt();
-            Level = reader.GetInt();
             Exp = reader.GetInt();
-            SkillPoint = reader.GetInt();
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.Put(GuildId);
-            writer.Put(Level);
             writer.Put(Exp);
-            writer.Put(SkillPoint);
         }
     }
 }
