859b3d568e2ef6a0b5aa9a57f9bb5b1ed3f6a50e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-15 16:07:20 +0700

    improve: database user login setup workflow

--- Scripts/MMOGame/Database/DatabaseUserLoginSetup.cs
+++ Scripts/MMOGame/Database/DatabaseUserLoginSetup.cs
@@ -3,30 +3,41 @@ using UnityEngine;
 namespace MultiplayerARPG.MMO
 {
     [DisallowMultipleComponent]
-    public class DatabaseUserLoginSetup : MonoBehaviour, IDatabaseUserLogin
+    public class DatabaseUserLoginSetup : MonoBehaviour
     {
-        public string GenerateNewId()
-        {
-            return GenericUtils.GetUniqueId();
-        }
+        public static readonly string LogTag = nameof(DatabaseUserLoginSetup);
 
-        public string GetHashedPassword(string password)
+        private class OldDatabaseUserLogin : IDatabaseUserLogin
         {
-            return password.PasswordHash();
-        }
+            public string GenerateNewId()
+            {
+                return GenericUtils.GetUniqueId();
+            }
 
-        public bool VerifyPassword(string password, string hashedPassword)
-        {
-            return password.PasswordVerify(hashedPassword);
+            public string GetHashedPassword(string password)
+            {
+                return password.PasswordHash();
+            }
+
+            public bool VerifyPassword(string password, string hashedPassword)
+            {
+                return password.PasswordVerify(hashedPassword);
+            }
         }
 
         void Start()
         {
             BaseDatabase database = GetComponent<BaseDatabase>();
             if (database.UserLoginManager == null)
+            {
+                database.LogInformation(LogTag, "`UserLoginManager` not setup yet, Get it in children to setup...");
                 database.UserLoginManager = GetComponentInChildren<IDatabaseUserLogin>();
+            }
             if (database.UserLoginManager == null)
-                database.UserLoginManager = this;
+            {
+                database.LogInformation(LogTag, "`UserLoginManager` not setup yet, Use default one...");
+                database.UserLoginManager = new OldDatabaseUserLogin();
+            }
         }
     }
 }
