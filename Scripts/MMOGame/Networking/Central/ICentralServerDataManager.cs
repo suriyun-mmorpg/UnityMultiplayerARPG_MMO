2684b3351e687b9f9e51c056e99ea49ef45aba9d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-27 00:09:09 +0700

    feat: WIP moving codes to shared repo

--- Scripts/MMOGame/Networking/Central/ICentralServerDataManager.cs
+++
@@ -1,9 +0,0 @@
-namespace MultiplayerARPG.MMO
-{
-    public interface ICentralServerDataManager
-    {
-        string GenerateCharacterId();
-        bool CanCreateCharacter(int dataId, int entityId, int factionId);
-        void SetNewPlayerCharacterData(PlayerCharacterData playerCharacterData, string characterName, int dataId, int entityId, int factionId);
-    }
-}
