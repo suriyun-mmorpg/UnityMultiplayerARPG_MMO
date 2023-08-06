570381c36aaf4876ebcb0c9e02796625f6d55166
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-08-06 22:57:22 +0700

    fix: wrong condition in-case that developer not setup factions

--- Scripts/MMOGame/Networking/Central/CentralServerDataManager.cs
+++ Scripts/MMOGame/Networking/Central/CentralServerDataManager.cs
@@ -16,7 +16,40 @@ namespace MultiplayerARPG.MMO
 
         public bool CanCreateCharacter(int dataId, int entityId, int factionId, IList<CharacterDataBoolean> publicBools, IList<CharacterDataInt32> publicInts, IList<CharacterDataFloat32> publicFloats)
         {
-            return GameInstance.PlayerCharacters.ContainsKey(dataId) && GameInstance.PlayerCharacterEntities.ContainsKey(entityId) && (GameInstance.Factions.Count <= 0 || (GameInstance.Factions.ContainsKey(factionId) && !GameInstance.Factions[factionId].IsLocked));
+            if (!GameInstance.PlayerCharacters.ContainsKey(dataId))
+            {
+                // No player character data
+                return false;
+            }
+
+            if (!GameInstance.PlayerCharacterEntities.ContainsKey(entityId))
+            {
+                // No player character entity
+                return false;
+            }
+
+            if (GameInstance.Factions.Count <= 0)
+            {
+                // No factions to select
+                return true;
+            }
+
+            if (GameInstance.Factions.ContainsKey(factionId) && !GameInstance.Factions[factionId].IsLocked)
+            {
+                // Can select the faction
+                return true;
+            }
+
+            foreach (Faction faction in GameInstance.Factions.Values)
+            {
+                if (!faction.IsLocked)
+                {
+                    // Found a faction which selectable (not locked), but the player does not select it, so determine that the player is selecting wrong faction
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public void SetNewPlayerCharacterData(PlayerCharacterData playerCharacterData, string characterName, int dataId, int entityId, int factionId, IList<CharacterDataBoolean> publicBools, IList<CharacterDataInt32> publicInts, IList<CharacterDataFloat32> publicFloats)
