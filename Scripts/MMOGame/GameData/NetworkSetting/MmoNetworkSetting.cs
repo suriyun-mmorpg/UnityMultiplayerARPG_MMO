a51902f9ee269c270b79eacb569b1a95bc6e73be
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-09-06 07:33:46 +0700

    improve: add multilanguge title, description and icon

--- Scripts/MMOGame/GameData/NetworkSetting/MmoNetworkSetting.cs
+++ Scripts/MMOGame/GameData/NetworkSetting/MmoNetworkSetting.cs
@@ -4,9 +4,8 @@ using UnityEngine.Events;
 namespace MultiplayerARPG.MMO
 {
     [CreateAssetMenu(fileName = GameDataMenuConsts.MMO_NETWORK_SETTING_FILE, menuName = GameDataMenuConsts.MMO_NETWORK_SETTING_MENU, order = GameDataMenuConsts.MMO_NETWORK_SETTING_ORDER)]
-    public class MmoNetworkSetting : ScriptableObject
+    public class MmoNetworkSetting : BaseGameData
     {
-        public string title = "Local Server";
         public string networkAddress = "127.0.0.1";
         public int networkPort = 6000;
     }
--- Scripts/MMOGame/UI/UIMmoServerEntry.cs
+++ Scripts/MMOGame/UI/UIMmoServerEntry.cs
@@ -1,54 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 namespace MultiplayerARPG.MMO
 {
     public class UIMmoServerEntry : UISelectionEntry<MmoNetworkSetting>
     {
-        [System.Obsolete("Deprecated, use `uiTextUsername` instead.")]
-        [HideInInspector]
-        public Text textTitle;
         public TextWrapper uiTextTitle;
-
-        protected override void Awake()
-        {
-            base.Awake();
-            MigrateTextComponent();
-        }
-
-#if UNITY_EDITOR
-        private void OnValidate()
-        {
-            if (MigrateTextComponent())
-                EditorUtility.SetDirty(this);
-        }
-#endif
-
-        public bool MigrateTextComponent()
-        {
-            bool hasChanges = false;
-            TextWrapper wrapper;
-#pragma warning disable CS0618 // Type or member is obsolete
-            if (textTitle != null)
-            {
-                hasChanges = true;
-                wrapper = textTitle.gameObject.GetOrAddComponent<TextWrapper>();
-                wrapper.unityText = textTitle;
-                uiTextTitle = wrapper;
-                textTitle = null;
-            }
-#pragma warning restore CS0618 // Type or member is obsolete
-            return hasChanges;
-        }
+        public TextWrapper uiTextDescription;
+        public Image imageIcon;
 
         protected override void UpdateData()
         {
             if (uiTextTitle != null)
-                uiTextTitle.text = Data.title;
+                uiTextTitle.text = Data.Title;
+
+            if (uiTextDescription != null)
+                uiTextDescription.text = Data.Description;
+
+            if (imageIcon != null)
+            {
+                Sprite iconSprite = Data == null ? null : Data.Icon;
+                imageIcon.gameObject.SetActive(iconSprite != null);
+                imageIcon.sprite = iconSprite;
+                imageIcon.preserveAspect = true;
+            }
         }
     }
 
