df11561e4048753d44927f320ff6b0533ceecd9e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-12-30 04:29:46 +0700

    feat: add `RESTIAPReceiptValidator` component, it will connects to this service https://github.com/suriyun-mmorpg/mmorpg-kit-iap-validation-service.git for validating

---
+++ Scripts/MMOGame/Networking/Map/IAPReceiptValidator/RESTIAPReceiptValidator.cs
@@ -0,0 +1,29 @@
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityRestClient;
+
+namespace MultiplayerARPG.MMO
+{
+    public class RESTIAPReceiptValidator : MonoBehaviour, IIAPReceiptValidator
+    {
+        public string apiUrl = "http://localhost:9802";
+        public string secretKey = "secret";
+
+        public async UniTask<IAPReceiptValidateResult> ValidateIAPReceipt(CashPackage cashPackage, string userId, string characterId, string receipt)
+        {
+            Dictionary<string, object> form = new Dictionary<string, object>
+            {
+                { "userId", userId },
+                { "characterId", characterId },
+                { "receipt", receipt },
+                { "packageId", cashPackage.Id },
+            };
+            RestClient.Result result = await RestClient.Post(RestClient.GetUrl(apiUrl, "/internal/iap-validate"), form, secretKey);
+            return new IAPReceiptValidateResult()
+            {
+                IsSuccess = !result.IsError(),
+            };
+        }
+    }
+}
